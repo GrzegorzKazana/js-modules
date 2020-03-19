fdb1eb92794310add0e9ff5cbc1d7539f8722ef3
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-03-19 20:42:12 +0100

    feat: iife-umd slides

--- public/iife/main.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  const sumText = document.getElementById("add");
-  const subText = document.getElementById("sub");
-
-  sumText.innerText = `2 + 2 = ${sum(2, 2)}`;
-  subText.innerText = `2 - 2 = ${sub(2, 2)}`;
-})();
