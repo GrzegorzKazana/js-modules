1d3c948840599eb74e2e44e1a615f01dd60cb908
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-03-19 19:17:37 +0100

    chore: slides set up

--- public/fileMD/main.js
+++
@@ -1,5 +0,0 @@
-const sumText = document.getElementById("add");
-const subText = document.getElementById("sub");
-
-sumText.innerText = `2 + 2 = ${sum(2, 2)}`;
-subText.innerText = `2 - 2 = ${sub(2, 2)}`;
